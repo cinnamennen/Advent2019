ff6dd8c1fc0b057cdff021890a5c39d78539b2b3
Caleb Mennen <cmennen@cisco.com>
2019-12-05 11:11:27 -0500

    ignore, day 5 init

---
+++ 05/tests.py
@@ -0,0 +1,27 @@
+import importlib
+import unittest
+
+a_solve = getattr(importlib.import_module('05.a'), 'solve')
+# b_solve = getattr(importlib.import_module('05.b'), 'test')
+
+
+class TestAExamples(unittest.TestCase):
+    def test_one(self):
+        self.assertTrue(a_solve('111111'))
+
+    def test_two(self):
+        self.assertFalse(a_solve('223450'))
+
+    def test_three(self):
+        self.assertFalse(a_solve('123789'))
+
+
+class TestBExamples(unittest.TestCase):
+    def test_one(self):
+        self.assertTrue(b_solve('112233'))
+
+    def test_two(self):
+        self.assertFalse(b_solve('123444'))
+
+    def test_three(self):
+        self.assertTrue(b_solve('111122'))
